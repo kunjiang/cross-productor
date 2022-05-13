4dda20929222b8c7d80d6298370cf51c50d378e8
jk <jk@jklib.org>
2022-05-13 12:28:08 +0800

    添加单元测试 parseInt 的算法有误: 1> 超出10进制会存在表达式问题 2> parseInt 超出 36 进制会返回 NaN

--- test/index.js
+++ test/index.js
@@ -1,15 +1,41 @@
 const fns = require('../index');
 
+// let data = [
+//   [ '白色' ],
+//   [ 19 ]
+// ];
+
+// let data = [
+//   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ],
+//   [ 1 ]
+// ];
+
+// let data = [
+//   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+//   [ 1, 2 ]
+// ];
+
+// let data = [
+//   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ],
+//   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ],
+// ];
+
+
 let data = [
-  [ '白色' ],
-  [ 19 ]
+  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+    31, 32, 33, 34, 35, 36, 37
+  ],
+  [ 1 ]
 ];
 
 
+
 let res1 = fns.crossProductor( data );
-let res2 = fns.crossProductor( data.map(Object.entries) );
+// let res2 = fns.crossProductor( data.map(Object.entries) );
 
 
 console.log(res1);
-console.log(res2);
+// console.log(res2);
 
